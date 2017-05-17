fff31c513a7c6041cc58b53c469373370c96c3a3
Jaakko Karhu <jaakko.st.karhu@gmail.com>
2017-05-17 13:59:10 +0200

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/lib/Button';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
   render() {
@@ -13,6 +15,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button>
+          Lol
+        </Button>
       </div>
     );
   }
