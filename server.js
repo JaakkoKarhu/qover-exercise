7b4d3b643015637acaef36b7198a736514ce6379
Jaakko Karhu <jaakko.st.karhu@gmail.com>
2017-05-18 00:30:39 +0200

    add quotes model, add /quotes get and post endpoints

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict'
 
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+var express = require('express')
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+var Quote = require('./model/quotes')
 
 var app = express()
 var router = express.Router()
@@ -10,6 +11,8 @@ var router = express.Router()
 var port = process.env.API_PORT || 3001
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+mongoose.connect('mongodb://localhost/qover-exercise')
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -28,6 +31,36 @@ app.use(function(req, res, next) {
 router.get('/', function(req, res) {
   res.json({ message: 'API INITIALIZED!'})
 })
+
+router.route('/quotes')
+  .get(function(req, res) {
+    Quote.find(function(err, quotes) {
+      if (err) {
+        res.json(err)
+      } else {
+        res.json(quotes)
+      }
+    })
+  })
+  .post(function(req, res) {
+    var quote = new Quote(),
+        { user, brand, carPrice, rejected, offer } = req.body,
+        date = Date.now()
+    quote.user = user || null
+    quote.brand = brand || null
+    quote.carPrice = carPrice || null
+    quote.rejected = rejected || null
+    quote.offer = offer || null
+    quote.date = date || null
+    quote.save(function(err) {
+      if (err) {
+        res.json(err)
+      } else {
+        res.json({ message: 'Quote added succesfully' })
+      }
+    })
+  })
+
 app.use('/api', router)
 app.listen(port, function() {
   console.log(`api running on port ${port}`)
