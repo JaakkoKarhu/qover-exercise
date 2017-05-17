21e43b8f1c9577f1a058abc6560d697ff9d5f4e3
Jaakko Karhu <jaakko.st.karhu@gmail.com>
2017-05-17 17:40:17 +0200

    add form validation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import 'react-select/dist/react-select.css';
+import Alert from 'react-bootstrap/lib/Alert';
 import Button from 'react-bootstrap/lib/Button';
 import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 import Col from 'react-bootstrap/lib/Col';
+import Clearfix from 'react-bootstrap/lib/Clearfix';
 import FormControl from 'react-bootstrap/lib/FormControl';
 import FormGroup from 'react-bootstrap/lib/FormGroup';
 import Grid from 'react-bootstrap/lib/Grid';
 import Select from 'react-select';
+import Row from 'react-bootstrap/lib/Row';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'react-select/dist/react-select.css';
 
 const carOptions = [
   // Double check these values
@@ -31,26 +34,84 @@ const carOptions = [
   },
 ]
 
-const submitHandler = () => {
-  console.log('submit')
+const rejectReasons = {
+  priceRange: 'Allowed price range for the car is between 5.000€ and 75.000€.',
+  missingBrand: 'Enter maker of the car.',
+  missingPrice: 'Enter price of the car.'
 }
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state={
-      selectedBrand: null
+      // Nested component state is clumsy, but reducer would be an overkill
+      inputs: {
+        name: { value: null, reject: false },
+        brand: { value: null, reject: false  },
+        price: { value: '', reject: false }
+      }
     }
   }
 
-  brandInputHandler = (val) => {
-    this.setState({
-      selectedBrand: val
+  handleBrandChange = (value) => {
+    let inputs = { ...this.state.inputs }
+    inputs.brand = { value, reject: false }
+    this.setState({ inputs })
+  }
+
+  getErrorMessages = () => {
+    const { inputs } = this.state
+    let messages = []
+    Object.keys(inputs).forEach((key) => {
+      const reject = inputs[key].reject
+      if (reject) {
+        let elem = <p key={ key }>{reject}</p>
+        messages.push(elem)
+      }
     })
+    if (messages.length>0) {
+      return <Alert bsStyle='danger'>{ messages }</Alert>
+    } else {
+      return null
+    }
+  }
+
+  handleNameChange = (value) => {
+    let inputs = { ...this.state.inputs }
+    inputs.name.value = value
+    this.setState({ inputs })
+  }
+
+  handlePriceChange = (value) => {
+    let inputs = { ...this.state.inputs }
+    if (isNaN(value)||(value.length===1&&value==='0')) {
+      return
+    } else if ((value.length===1&&value>7)||value>75000) {
+      let reject =  rejectReasons.priceRange
+      inputs.price = { value, reject }
+    } else {
+      let reject = false;
+      inputs.price = { value, reject }
+    }
+    this.setState({ inputs })
+  }
+
+  handleSubmit = () => {
+    const { brand, price } = this.state.inputs
+    let inputs = { ...this.state.inputs }
+    if (!brand.value) {
+      inputs.brand.reject = rejectReasons.missingBrand
+    }
+    if (!price.value) {
+      inputs.price.reject = rejectReasons.missingPrice
+    } else if (price.value<5000||price.value>75000) {
+      inputs.price.reject = rejectReasons.priceRange
+    }
+    this.setState({inputs})
   }
 
   render() {
-    const  { selectedBrand } = this.state
+    const { brand, price } = this.state.inputs
     return (
       <div className="App">
         <Grid>
@@ -58,31 +119,38 @@ class App extends Component {
                sm={ 6 }>
             <FormGroup>
               <ControlLabel>
-                Name of the driver:
+                Name of the driver
               </ControlLabel>
-              <FormControl />
+              <FormControl placeholder="First name, last name"
+                           onChange={ (e) => this.handleNameChange(e.target.value) } />
             </FormGroup>
-            <FormGroup>
+            <FormGroup className={ brand.reject ? 'has-error' : '' }>
               <ControlLabel>
                 Car brand
               </ControlLabel>
-              <Select value={ selectedBrand }
+              <Select value={ brand.value }
                       clearable={ false }
                       options={ carOptions }
-                      onChange={ this.brandInputHandler }>
+                      onChange={ this.handleBrandChange }>
               </Select>
             </FormGroup>
-            <FormGroup>
+            <FormGroup className={ price.reject ? 'has-error' : '' }>
               <ControlLabel>
                 Price at the time of purchase (including VAT)
               </ControlLabel>
-              <FormControl />
+              <FormControl placeholder="5.000-75.000€"
+                           value={ price.value }
+                           onChange={ (e) => this.handlePriceChange(e.target.value) } />
             </FormGroup>
-            <Button bsStyle="success"
-                    className="pull-right"
-                    onClick={ submitHandler }>
-              Get price!
-            </Button>
+            <FormGroup>
+              <Button bsStyle="success"
+                      className="pull-right"
+                      onClick={ this.handleSubmit }>
+                Get price!
+              </Button>
+            </FormGroup>
+            <Clearfix />
+            { this.getErrorMessages() }
           </Col>
         </Grid>
       </div>
